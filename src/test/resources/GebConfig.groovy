536de539c1a79a171e947251f16c4e6b6b8e370b
Mike Calvo <mike.calvo@target.com>
2016-03-13 10:50:03 -0500

    Added support for chrome

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -3,9 +3,18 @@
 
     See: http://www.gebish.org/manual/current/#configuration
 */
+
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.DesiredCapabilities
 
 waiting {
   timeout = 2
 }
-driver = { new FirefoxDriver() }
+
+driver = {
+  def path = System.properties['os.name'].toLowerCase().contains('windows') ?
+      'node_modules\\.bin\\chromedriver.exe' : './node_modules/.bin/chromedriver'
+  System.setProperty("webdriver.chrome.driver", path);
+  System.properties['browser']?.toString()?.toLowerCase() == 'chrome' ? new ChromeDriver(new DesiredCapabilities()) : new FirefoxDriver()
+}
